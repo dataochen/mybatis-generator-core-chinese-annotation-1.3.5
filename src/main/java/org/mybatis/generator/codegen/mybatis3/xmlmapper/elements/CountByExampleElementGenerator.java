343742aeafd8a579ce93e5321263ecb34825faac
egg_CN <1608384702@qq.com>
2017-11-24 18:22:51 +0800

    do sql记录

--- src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/CountByExampleElementGenerator.java
+++ src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/CountByExampleElementGenerator.java
@@ -15,14 +15,15 @@
  */
 package org.mybatis.generator.codegen.mybatis3.xmlmapper.elements;
 
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.codegen.mybatis3.ListUtilities;
+import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
 
 /**
- *
  * @author Jeff Butler
- *
  */
 public class CountByExampleElementGenerator extends AbstractXmlElementGenerator {
 
@@ -33,8 +34,8 @@ public class CountByExampleElementGenerator extends AbstractXmlElementGenerator 
     @Override
     public void addElements(XmlElement parentElement) {
         XmlElement answer = new XmlElement("select"); //$NON-NLS-1$
-
-        String fqjt = introspectedTable.getExampleType();
+        //fixme 这里是parameterType 参数 分页查询参数
+        String fqjt = introspectedTable.getBaseRecordType();
 
         answer.addAttribute(new Attribute(
                 "id", introspectedTable.getCountByExampleStatementId())); //$NON-NLS-1$
@@ -47,8 +48,32 @@ public class CountByExampleElementGenerator extends AbstractXmlElementGenerator 
         sb.append("select count(*) from "); //$NON-NLS-1$
         sb.append(introspectedTable
                 .getAliasedFullyQualifiedTableNameAtRuntime());
+        sb.append(" WHERE 1=1 ");
         answer.addElement(new TextElement(sb.toString()));
-        answer.addElement(getExampleIncludeElement());
+
+
+        for (IntrospectedColumn introspectedColumn : ListUtilities.removeGeneratedAlwaysColumns(introspectedTable
+                .getNonPrimaryKeyColumns())) {
+            XmlElement isNotNullElement = new XmlElement("if"); //$NON-NLS-1$
+            sb.setLength(0);
+            sb.append(introspectedColumn.getJavaProperty());
+            sb.append(" != null"); //$NON-NLS-1$
+            isNotNullElement.addAttribute(new Attribute("test", sb.toString())); //$NON-NLS-1$
+            answer.addElement(isNotNullElement);
+
+            sb.setLength(0);
+            sb.append(" AND ");
+            sb.append(MyBatis3FormattingUtilities
+                    .getEscapedColumnName(introspectedColumn));
+            sb.append(" = "); //$NON-NLS-1$
+            sb.append(MyBatis3FormattingUtilities
+                    .getParameterClause(introspectedColumn));
+            isNotNullElement.addElement(new TextElement(sb.toString()));
+        }
+
+
+
+//        answer.addElement(getExampleIncludeElement());
 
         if (context.getPlugins().sqlMapCountByExampleElementGenerated(
                 answer, introspectedTable)) {
